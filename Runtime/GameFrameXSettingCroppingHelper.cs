e4ddcaa04d3b9df4cb4f1d91aae2994ebb8a5072
AlianBlank <wangfj11@foxmail.com>
2024-04-09 18:48:48 +0800

    [修改]1. 修改Setting的功能拆分和文档

---
+++ Runtime/GameFrameXSettingCroppingHelper.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace GameFrameX.Setting.Runtime
+{
+    public class GameFrameXSettingCroppingHelper : MonoBehaviour
+    {
+        private void Start()
+        {
+            _ = typeof(DefaultSetting);
+            _ = typeof(DefaultSettingHelper);
+            _ = typeof(DefaultSettingSerializer);
+            _ = typeof(PlayerPrefsSettingHelper);
+            _ = typeof(SettingComponent);
+            _ = typeof(SettingHelperBase);
+            _ = typeof(ISettingHelper);
+            _ = typeof(ISettingManager);
+            _ = typeof(SettingManager);
+        }
+    }
+}
